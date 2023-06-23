9391ffce1f81b46f9ecdb00afa4da667d99ae8b6
Jaimendez23 <105337922+Jaimendez23@users.noreply.github.com>
2023-06-23 23:36:10 +0800

    Create salary by profession and identifying if theres a gender pay gap.sql

---
+++ sql/salary_data/salary by profession and identifying if theres a gender pay gap.sql
@@ -0,0 +1,64 @@
+--create table 
+
+Create table salaryData(
+	age smallint,
+	gender varchar(10),
+	educational_lvl varchar(50),
+	job_title varchar(100),
+	years_of_exp smallint,
+	salary float
+)
+
+-- import csv
+COPY salaryData FROM 'C:\Users\JAI\Desktop\data analyst journey\Ready for Resume\sql\salary_data\salary_Data.csv' CSV HEADER;
+
+--check data 
+select * from salarydata
+
+--updating data to remove inconsistency
+UPDATE salaryData
+SET educational_lvl = 
+  CASE
+    WHEN educational_lvl = 'phD' THEN 'PhD'
+    WHEN educational_lvl = 'Master''s' THEN 'Master''s Degree'
+    WHEN educational_lvl = 'Bachelor''s' THEN 'Bachelor''s Degree'
+    ELSE NULL
+  END
+WHERE educational_lvl IN ('phD', 'Master''s', 'Bachelor''s', NULL);
+
+
+""" avg salary for each profession with less than 1 year of exp. also indicated 
+		the lower end and higher end of the salary"""
+select distinct(job_title), avg(salary) as sal, years_of_exp, max(salary) as higherEnd, min(salary) as lowerEnd from salarydata
+where years_of_exp <= 1
+group by 1,3
+order by sal desc
+
+ 
+select job_title, max(salary)  from salarydata 
+where years_of_exp = 1 
+group by 1
+limit 1
+
+-- professions with lowest salary with 1 year exp
+select job_title, min(salary)  from salarydata 
+where years_of_exp = 1 
+group by 1
+limit 1
+
+
+-- gender pay gap if any 
+SELECT job_title,
+       AVG(CASE WHEN gender = 'Male' THEN salary END) AS avg_male_salary,
+       AVG(CASE WHEN gender = 'Female' THEN salary END) AS avg_female_salary
+FROM salarydata
+WHERE years_of_exp = 2
+GROUP BY job_title;
+
+
+-- avg salary by educational level and years of exp. 
+SELECT DISTINCT job_title, educational_lvl , avg(salary)
+FROM salarydata
+WHERE educational_lvl = 'Bachelor''s Degree' and years_of_exp = 1
+group by job_title, educational_lvl
+order by 3 desc
